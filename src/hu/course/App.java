095ac016aa113815249a5e74aafe3e24d1aecb95
pfazekas <v-pfazekas@hotels.com>
2022-01-12 09:25:11 +0100

    Tasks 3-7.

--- src/hu/course/App.java
+++ src/hu/course/App.java
@@ -8,10 +8,14 @@ import java.util.Scanner;
 public class App {
 
     private final CourseService courseService;
+    private final Console console;
+    private final DataWriter dataWriter;
 
     private App() {
         DataApi dataApi = new DataApi(new FileReader(), new DataParser());
         courseService = new CourseService(dataApi.getData("beosztas.txt"));
+        console = new Console(new Scanner(System.in));
+        dataWriter = new DataWriter("of.txt");
     }
 
     public static void main(String[] args) {
@@ -20,6 +24,24 @@ public class App {
 
     private void run() {
         System.out.println("2. feladat:");
-        System.out.println("A fájlban " + courseService.getCourseCount() +" bejegyzés van.");
+        System.out.println("A fájlban "
+                + courseService.getCourseCount() +" bejegyzés van.");
+        System.out.println("3. feladat:");
+        System.out.println("Az iskolában a heti összóraszám: "
+                + courseService.getTotalLessonCount());
+        System.out.println("4. feladat:");
+        System.out.print("Egy tanár neve: ");
+        String name = console.read();
+        System.out.println("A tanár heti óraszáma: "
+                + courseService.getTeacherWeeklyLesson(name));
+        dataWriter.write(courseService.getMasterTeachers());
+        System.out.println("6. feladat");
+        System.out.print("Osztály: ");
+        String classId = console.read();
+        System.out.print("Tantárgy: ");
+        String subject = console.read();
+        System.out.println(courseService.getCourseStatus(subject, classId));
+        System.out.println("7. feladat:");
+        System.out.println("Az iskolában " + courseService.getTeacherCount() + " tanár tanít. ");
     }
 }
--- src/hu/course/controller/CourseService.java
+++ src/hu/course/controller/CourseService.java
@@ -20,4 +20,57 @@ public class CourseService {
         return courses.size();
     }
 
+    /**
+     * 3. feladat
+     */
+    public int getTotalLessonCount() {
+        return courses.stream()
+                .mapToInt(Course::getWeeklyLessonNumber)
+                .sum();
+    }
+
+    /**
+     * 4. feladat
+     */
+    public int getTeacherWeeklyLesson(String name) {
+        return courses.stream()
+                .filter(course -> course.getTeacher().equals(name))
+                .mapToInt(Course::getWeeklyLessonNumber)
+                .sum();
+    }
+
+    /**
+     * 5. feladat
+     */
+    public List<String> getMasterTeachers() {
+        return courses.stream()
+                .filter(course -> "osztalyfonoki".equals(course.getSubject()))
+                .map(course -> String.format("%s - %s", course.getClassId(), course.getTeacher()))
+                .collect(Collectors.toList());
+    }
+
+    /**
+     * 6. feladat
+     */
+    public String getCourseStatus(String subject, String classId) {
+        return getCertainCourseCount(subject, classId) == 1
+                ? "Osztályszinten tanulják."
+                : "Csoportbontásban tanulják.";
+    }
+
+    private long getCertainCourseCount(String subject, String classId) {
+        return courses.stream()
+                .filter(i -> i.getSubject().equals(subject) && i.getClassId().equals(classId))
+                .count();
+    }
+
+    /**
+     * 7. feladat
+     */
+    public long getTeacherCount() {
+        return courses.stream()
+                .map(Course::getTeacher)
+                .distinct()
+                .count();
+    }
 }
